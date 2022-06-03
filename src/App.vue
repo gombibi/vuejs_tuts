0637e7ce746947ef5f1f99d6c21db01c3d349fd2
DESKTOP-9UTVCDM\user <bb.style.on@gmail.com>
2022-06-03 17:52:42 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+      <Modal theme="" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>bb Givaway!</h1>
+        <p>Grab your swag for half price!</p>
+      </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModalTwo">
+      <Modal @close="toggleModalTwo">
+        <h1>Sign up to the newsletter</h1>
+        <p>For updates and promo codes!</p>
+      </Modal>
+  </teleport>
+
+  <button @click.alt="toggleModal">open modal(alt)</button>
+  <button @click="toggleModalTwo">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//challange
+//  - create an extra button to open a different modal
+//  - use the same modal component but pass in a different template (slot)
+//  - use a different method(e.g. toggleModalTwo) and data (e.g. showModalTwo) 
 
-export default {
+import Modal from './components/Modal.vue'
+
+export default {   
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data(){
+    return{
+      title: 'My First Vue App :)',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods:{
+    toggleModal(){
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo;
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
